e9b07e0a3c872a62101859cbdc633dc8a807ce96
Scriptor25 <f.schreiber.2006@gmail.com>
2026-07-29 15:06:13 +0200

    added empty java source dirs, removed unnecessary inline modifier on log function

--- server/src/main/kotlin/dev/scriptor/server/Log.kt
+++ server/src/main/kotlin/dev/scriptor/server/Log.kt
@@ -3,6 +3,6 @@ package dev.scriptor.server
 import java.util.logging.Level
 import java.util.logging.Logger
 
-inline fun Logger.trace(t: Throwable) {
+fun Logger.trace(t: Throwable) {
     this.log(Level.SEVERE, t.stackTraceToString())
 }
