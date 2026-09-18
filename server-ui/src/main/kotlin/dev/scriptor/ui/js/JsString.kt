99bc298dfdcbcdfc02e1f7107c4ecba16d4c25ec
Scriptor25 <f.schreiber.2006@gmail.com>
2026-09-19 00:49:40 +0200

    escape string js

--- server-ui/src/main/kotlin/dev/scriptor/ui/js/JsString.kt
+++ server-ui/src/main/kotlin/dev/scriptor/ui/js/JsString.kt
@@ -1,9 +1,20 @@
 package dev.scriptor.ui.js
 
+fun escapeString(str: String): String = buildString {
+    for (char in str) {
+        if (char != '"') {
+            append(char)
+            continue
+        }
+
+        append("\\\"")
+    }
+}
+
 class JsString(
     val value: String,
 ) : JsExpression {
 
     // TODO: escape string value
-    override fun toJsString(): String = """"$value""""
+    override fun toJsString(): String = """"${escapeString(value)}""""
 }
